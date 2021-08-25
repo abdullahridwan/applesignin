3ed68daf5153ebacef4a96822ce42040904f3fa1
Abdullah Ridwan <abdullahridwan73@icloud.com>
2021-08-25 11:31:58 -0400

    Initial Commit

---
+++ Shared/apple_signin_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_signin_testApp.swift
+//  Shared
+//
+//  Created by Abdullah Ridwan on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct apple_signin_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
